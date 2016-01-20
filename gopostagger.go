76c7f0d12b6364f91e2cfd6b2a6212da603ccb43
Antonio Linari <advancedlogic@me.com>
2016-01-20 11:22:46 +0100

    Update gopostagger.go

--- gopostagger.go
+++ gopostagger.go
@@ -1,24 +1,42 @@
 package main
 
 import (
+	"strings"
 	"time"
 
-	. "github.com/advancedlogic/go-postagger/engine"
+	"github.com/alecthomas/kingpin"
+)
+
+var (
+	train = kingpin.Flag("train", "Enable train model").Short('t').Bool()
+	model = kingpin.Flag("model", "HMM Trained Model").Short('m').Required().String()
+	text  = kingpin.Flag("text", "Text to process").Short('t').Required().String()
 )
 
 func main() {
-	start := time.Now().UnixNano()
-	hmmParser := NewHMMParser("simple.pos")
-	hmmParser.Load()
-	hmm := NewHMM(hmmParser)
-	wordSequence := hmmParser.FWordSequence()
-	ws := make([]string, wordSequence.Len())
-	cont := 0
-	for w := wordSequence.Front(); w != nil; w = w.Next() {
-		ws[cont] = w.Value.(string)
-		cont++
+	kingpin.Parse()
+	if *train {
+		println("START")
+		start := time.Now().UnixNano()
+		ptrain := NewHMMParser("training/training.set")
+		println("Training set loaded")
+		ptrain.fParseTrainer()
+		println("Training set trained")
+		ptrain.Save()
+		stop := time.Now().UnixNano()
+		println("training delta", (stop-start)/1000, "mics")
+	} else {
+		println("Loading model " + *model)
+		hmmParser := NewHMMParser(*model)
+		hmmParser.Load()
+		hmm := NewHMM(hmmParser)
+		println("Model " + *model + " loaded")
+
+		start := time.Now().UnixNano()
+		ws := strings.Split("<s> "+*text, " ")
+		hmm.fViterbi(ws)
+		println(hmm.String())
+		stop := time.Now().UnixNano()
+		println("test delta", (stop-start)/1000, "mics")
 	}
-	hmm.FViterbi(ws)
-	stop := time.Now().UnixNano()
-	println("test delta", (stop-start)/1000, "mics")
 }
